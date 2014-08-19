141a864ac9ea7ac0e10100bc01e3e2ffdd852a33
rfr03 <roger.franklin@bskby.com>
2014-08-19 21:48:05 +0100

    Adding handlebars

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 var app = require('express')();
+var exphbs  = require('express-handlebars');
+hbs = exphbs.create({ /* config */ });
+
+// Register `hbs.engine` with the Express app.
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 var http = require('http').Server(app);
 
 app.get('/', function(req, res){
-  res.sendfile('index.html');
+  res.render('index');
 });
 
 http.listen(3000, function(){
