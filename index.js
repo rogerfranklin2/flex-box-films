bc46abcff8b263fd82f96f041f5665d30311acd4
rfr03 <roger.franklin@bskby.com>
2014-08-28 18:49:53 +0100

    Will this push?

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var app = require('express')();
 var exphbs  = require('express-handlebars');
 hbs = exphbs.create({ /* config */ });
 
+var request = require('request');
+
 // Register `hbs.engine` with the Express app.
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
@@ -9,7 +11,17 @@ app.set('view engine', 'handlebars');
 var http = require('http').Server(app);
 
 app.get('/', function(req, res){
-  res.render('index');
+
+request({
+  method: 'GET',
+  url: 'http://private-09d29-themoviedb.apiary-mock.com/3/movie/popular',
+  headers: {
+    'Accept': 'application/json'
+  }}, function (error, response, body) {
+
+    res.render('index', { movies : JSON.parse(body)});
+  });
+
 });
 
 http.listen(3000, function(){
